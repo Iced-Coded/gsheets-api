8d5122b8eb0387892346bd32503aaf0c888963d2
Oleksandr Volkohon <volkogon212@gmail.com>
2024-12-05 11:59:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
-    "http://127.0.0.1:5500".
+    "http://127.0.0.1:5500",
     "https://ptime.infy.uk"
 ]
 
